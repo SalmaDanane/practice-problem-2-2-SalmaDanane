81d7424bffad754041c284fc00c7828562692f5b
SalmaDanane <343249595@tdsb.ca>
2026-06-02 10:55:03 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,29 @@
 public class PracticeProblem {
+    
+ public static boolean isEven(int num) {
+     return num % 2 == 0;
+     
+ } 
+ 
+ public static boolean isOdd(int num) {
+     
+     return num % 2 !0;
+ }
+ 
+public static boolean isPositive(int num){
+    
 
-	public static void main(String args[]) {
+return num > 0;
+}
+ 
+public static boolean isNegative(int num){
+    
+    return num < 0;
+}
 
-	}
-
+ public static in combinedLength(String word, String word2){
+     
+     return word.length() + word2.length();
+ }
+    
 }
